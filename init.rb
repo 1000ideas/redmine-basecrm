e980429ce6629f7720167a99060eb3544a97dd0e
Szczepan Bułat <szczepan@1000i.pl>
2016-04-14 00:07:09 +0200

    remove top menu link

--- init.rb
+++ init.rb
@@ -6,15 +6,6 @@ Redmine::Plugin.register :basecrm do
   url 'http://1000i.pl'
   author_url 'http://1000i.pl'
 
-  menu :top_menu,
-       :basecrm,
-       { controller: :deals, action: :check_for_new_deals },
-       caption: :top_menu_deals
-
-  project_module :basecrm do
-    permission :deals, deals: :check_for_new_deals
-  end
-
   settings default: { 'tracker_id' => Tracker.first.id, 'project_id' => Project.first.id },
            partial: 'deals/settings'
 end
