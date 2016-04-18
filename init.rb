881fa93b1f92411372d0bc91969bd8a32d7d8953
Szczepan Bułat <szczepan@1000i.pl>
2016-04-18 12:05:15 +0200

    can choose category in plugin settings

---
+++ init.rb
@@ -0,0 +1,15 @@
+Redmine::Plugin.register :basecrm do
+  name 'BaseCRM plugin'
+  author '1000ideas'
+  description 'Get your deals from BaseCRM as soon as they appear'
+  version '0.0.1'
+  url 'http://1000i.pl'
+  author_url 'http://1000i.pl'
+
+  settings default: { 'tracker_id' => Tracker.first.id,
+                      'project_id' => Project.first.id,
+                      'category_id' => nil,
+                      'base_token' => '',
+                      'device_uuid' => '' },
+           partial: 'deals/settings'
+end
