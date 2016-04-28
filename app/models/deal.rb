530421858d9f7cc776840ee3ba7a408a11fd92b5
Krzysztof Kosman <krzysztof@1000i.pl>
2016-04-28 19:55:59 +0200

    token removal

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -11,7 +11,7 @@ class Deal < ActiveRecord::Base
     rescue
       return { error: l(:client_error) }
     end
-    # '5dd38d5b675c56f9651b42ff66dde2d74971d0490c6af94aa66cf3e87b47b801')
+    
     begin
       sync = BaseCRM::Sync.new(
         client: client,
